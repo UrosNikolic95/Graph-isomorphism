f3d370e8fdfebb82aa87955c8f54119e4d3dd825
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-04-14 20:24:29 +0200

    array dictionary

---
+++ Test_for_algorithm/GraphFunctions/Array_Dictionary.cs
@@ -0,0 +1,45 @@
+
+
+
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace GraphFunctions
+{
+    public class Array_Dictionary
+    {
+        private Dictionary<int[], int> dic = new Dictionary<int[], int>(new ArrComp());
+        int counter = 0;
+        int getMarker(int[] arr)
+        {
+            if (!dic.ContainsKey(arr))
+            {
+                dic[arr] = counter++;
+            }
+            return dic[arr];
+        }
+
+    }
+
+    class ArrComp : IEqualityComparer<int[]>
+    {
+        public bool Equals(int[]? x, int[]? y)
+        {
+            if (x == null) return false;
+            if (y == null) return false;
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode([DisallowNull] int[] obj)
+        {
+            int bits = 32;
+            int hash = 0;
+            for (int i1 = 0; i1 < obj.Length; i1++)
+            {
+                hash ^= obj[i1];
+                hash = hash << 8 | hash >> bits - 8; //rotate by 8 bits
+            }
+            return hash;
+        }
+    }
+}
